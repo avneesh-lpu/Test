7a0c0b00f13a9e4706d2fc9cc96de215a1ca6f91
Vishnu Sai Nath Karumrui <vishnusai.karumuri@gmail.com>
2018-01-30 09:18:41 +0530

    commit 1

--- app/src/main/java/com/example/ambikesh/test/MainActivity.java
+++ app/src/main/java/com/example/ambikesh/test/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.ambikesh.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gv;
+    MyAdapter mad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
