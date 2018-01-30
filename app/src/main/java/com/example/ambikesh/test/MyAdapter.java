b5052201c674acf15480f5377ba77d91b0ad1096
Vishnu Sai Nath Karumrui <vishnusai.karumuri@gmail.com>
2018-01-30 09:30:19 +0530

    commit 2

--- app/src/main/java/com/example/ambikesh/test/MyAdapter.java
+++ app/src/main/java/com/example/ambikesh/test/MyAdapter.java
@@ -1,8 +1,11 @@
 package com.example.ambikesh.test;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by AMBIKESH on 30-Jan-18.
@@ -10,15 +13,30 @@ import android.widget.BaseAdapter;
 
 public class MyAdapter extends BaseAdapter
 {
+    Context ct;
+    ArrayList<Integer> al;
+    MyAdapter(Context c)
+    {
+        ct = c;
+        al = new ArrayList<>();
+        addItem();
+    }
+    void addItem()
+    {
+        for(int i=0;i<15;i++)
+        {
+            al.add(R.drawable.im);
+        }
+    }
     @Override
     public int getCount()
     {
-        return 0;
+        return al.size();
     }
     @Override
     public Object getItem(int position)
     {
-        return null;
+        return al.get(position);
     }
     @Override
     public long getItemId(int position)
@@ -28,6 +46,6 @@ public class MyAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        return null;
+      return null;
     }
 }
