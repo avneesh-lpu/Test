fbc88c850cd4cc659919dce18250207eed3decc2
Vishnu Sai Nath Karumrui <vishnusai.karumuri@gmail.com>
2018-01-30 09:47:38 +0530

    commit 3

--- app/src/main/java/com/example/ambikesh/test/MainActivity.java
+++ app/src/main/java/com/example/ambikesh/test/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ambikesh.test;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gv = findViewById(R.id.gv);
+        mad = new MyAdapter(this);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+                view.setBackgroundColor(Color.GREEN);
+            }
+        });
+        gv.setAdapter(mad);
     }
 }
